f01d062cfb0f46c4b5ad97d600d68e270e38acf1
botowner4 <botowner4@gmail.com>
2026-02-19 15:52:14 +0300

    Delete id.js

--- id.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-function makeid(len = 8) {
-  // returns an alphanumeric id of length `len`
-  return crypto.randomBytes(Math.ceil(len / 2)).toString('hex').slice(0, len);
-}
-
-module.exports = { makeid };
