f936987ced510fbf6eee276a9ceac4db00752958
botowner4 <botowner4@gmail.com>
2026-02-23 19:23:47 +0300

    Update botowner4.js

--- botowner4.js
+++ botowner4.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
+const __path = process.cwd();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const __path = process.cwd()
-const PORT = process.env.PORT || 8000;
-let server = require('./bugbotqr.js'),
-    code = require('./pair');
+
+const PORT = process.env.PORT || 10000;
+
+let serverQR = require('./bugbotqr.js');
+let codePair = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/bugbotqr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/botowner4page.html')
-})
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/bugbotqr', serverQR);
+app.use('/pair', codePair);
+
+app.get('/', async (req, res) => {
+    res.sendFile(__path + '/botowner4page.html');
+});
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star
+    console.log("Server running on port " + PORT);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
-/**
-    powered by BUGFIXED tech team 
-    join Whatsapp channel for more updates 
-    **/
+module.exports = app;
